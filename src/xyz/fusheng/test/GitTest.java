f5a32bed34ec3c6155d218f407bdbb3c8ec376a5
code-fusheng <2561035977@qq.com>
2020-12-24 16:52:13 +0800

    新增了GitTest类

---
+++ src/xyz/fusheng/test/GitTest.java
@@ -0,0 +1,12 @@
+package xyz.fusheng.test;
+
+/**
+ * @FileName: GitTest
+ * @Author: code-fusheng
+ * @Date: 2020/12/24 4:44 下午
+ * @Version: 1.0
+ * @Description: git测试类
+ */
+
+public class GitTest {
+}
