3b2814d1a972d1df92827a0ff3569fe63e64e2e2
code-fusheng <2561035977@qq.com>
2020-12-25 13:13:57 +0800

    git dev 分支测试

---
+++ src/xyz/fusheng/test/GitDevTest.java
@@ -0,0 +1,19 @@
+package xyz.fusheng.test;
+
+/**
+ * @FileName: GitDevTest
+ * @Author: code-fusheng
+ * @Date: 2020/12/25 1:11 下午
+ * @Version: 1.0
+ * @Description: Git 分支测试
+ */
+
+public class GitDevTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("这是 Git 的分支测试内容！");
+
+    }
+
+}
